82bb637c8e2da63eee3c0cb837627ca79b9b7995
Tyaz Gagaman <muhammad.118140077@student.itera.ac.id>
2023-06-24 21:49:59 +0700

    installing spring boot

---
+++ src/main/java/com/java/javaapichallenge/JavaApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.java.javaapichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/javaapichallenge/JavaApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.javaapichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
