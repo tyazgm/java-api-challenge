def8de200e243adf9107662f98d345127a1fa840
Tyaz Gagaman <muhammad.118140077@student.itera.ac.id>
2023-06-26 02:27:37 +0700

    add jsonproperty to CreateOrderRequest and UpdateOrderRequest model

--- src/main/java/com/java/javaapichallenge/orders/model/CreateOrderRequest.java
+++ src/main/java/com/java/javaapichallenge/orders/model/CreateOrderRequest.java
@@ -1,5 +1,6 @@
 package com.java.javaapichallenge.orders.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateOrderRequest {
+    @JsonProperty("customer_name")
     private String customerName;
+
+    @JsonProperty("total_amount")
     private Double totalAmount;
 }
